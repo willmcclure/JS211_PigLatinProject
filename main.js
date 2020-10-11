891ff46548a76572a88f99c43efbf23cf430eb23
@willmcclure <willbmcclure@gmail.com>
2020-10-11 18:48:53 -0500

    PigLatin GUI completed

--- main.js
+++ main.js
@@ -1,91 +1,51 @@
 'use strict';
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 // initialize array of vowels
 const vowels = ["a", "e", "i", "o", "u"];
 
-const pigLatin = (word) => {
+const pigLatin = () => {
+  let input = document.getElementById('input').value
+
+  const inputString = input.toString();
   // string to lowercase and trim
-  word = word.toLowerCase()
-  word = word.trim()
-  
+  input = inputString.toLowerCase()
+  input = input.trim()
+
   // initialize vowel index to 0
   let vowelIndex = 0;
 
   // 1st letter is vowel
-  if (vowels.includes(word[0])) {
-    return word + "yay";
+  if (vowels.includes(input[0])) {
+    document.getElementById('output').innerText = input + "yay";
+    return
     }
   // loop each letter
-  const loopWord = word => {
-    for (let i = 0; i < word.length; i++) {
-      if (vowels.includes(word[i]))
-      stop = true
-      break
+  for (let i = 0; i < input.length; i++) {
+      if (vowels.includes(input[i])){
+        vowelIndex = i
+        break
+      }
     }
-  }
 
-  let half1 = word.slice(0, vowelIndex)
-  let half2 = word.slice(vowelIndex)
+  let half1 = input.slice(0, vowelIndex)
+  let half2 = input.slice(vowelIndex)
 
-  let myWord = half1 + half2 + "ay"
-
-  return myWord
+  document.getElementById('output').innerText = half2 + half1 + "ay"
 }
 
-// the first function called in the program to get an input from the user
-// to run the function use the command: node main.js
-// to close it ctrl + C
-const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
+const trying = () => {
+  let input = document.getElementById('input').value
+  document.getElementById('output').innerText = input
 }
 
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
+// translate button from index.html
+let translationButton = document.getElementById('translate');
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
+// Click event on translate button to run text through PigLatin 
+translationButton.addEventListener('click', pigLatin)
 
-  getPrompt();
-
+// Clear input and display areas on index.html
+const reset = () => {
+  document.getElementById('input').value = "";
+  document.getElementById('output').innerText = "";
 }
-
-// **********
-//   HINTS
-// **********
-
-// break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
-// 2. if word begins in with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
